2cfef20912962fc31e4991a077d69f7af54bb014
Rashidi Zin <rashidi@zin.my>
2018-03-02 17:21:02 +0800

    Add unit test retrieve from actuator
    Signed-off-by: Rashidi Zin <rashidi@zin.my>

--- src/test/java/rz/demo/jdbc/repo/ApplicationTests.java
+++ src/test/java/rz/demo/jdbc/repo/ApplicationTests.java
@@ -3,18 +3,28 @@ package rz.demo.jdbc.repo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 import rz.demo.jdbc.repo.greet.GreetService;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@AutoConfigureMockMvc
 public class ApplicationTests {
 
     @Autowired
     private GreetService service;
+
+    @Autowired
+    private MockMvc mvc;
 
     @Test
     public void greetAndResponseContainsNameFromDatabase() {
@@ -23,5 +33,14 @@ public class ApplicationTests {
         assertThat(response)
                 .doesNotContain("Default")
                 .contains("Demo");
+    }
+
+    @Test
+    public void getConfigurationValueFromActuator() throws Exception {
+        mvc.perform(
+                get("/actuator/env/{key}", "app.greet.name")
+        )
+                .andExpect(jsonPath("$.property.source", is("configService:demo-default")))
+                .andExpect(jsonPath("$.property.value", is("Demo")));
     }
 }
